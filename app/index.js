3dc5f52904bb8e2659117bc3295998bd6aadecbc
zcfrank1st <zhchaos@gmail.com>
2014-05-14 13:10:56 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,49 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var ImpressjsGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the marvelous Impressjs generator!'));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: false
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.copy('impress.js', 'impress.js');
+    this.copy('slide.html', 'slide.html');
+    
+    console.log('Finishing Gernerating Impress Slide!');
+  },
+
+});
+
+module.exports = ImpressjsGenerator;
